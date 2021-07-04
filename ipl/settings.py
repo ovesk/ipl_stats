ba529b1646e3513e17fce9f520f8442f9ee55116
Oves Khan <oves.khan@amplussolar.com>
2021-07-04 22:21:27 +0530

    DB to read from environment variables in heroku

--- ipl/settings.py
+++ ipl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,14 +79,13 @@ WSGI_APPLICATION = 'ipl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deijumqkct6jh5',
-        'USER': 'xmhpjboluwyfpc',
-        'PASSWORD': 'bafd4058fb5048d4e8a6e76b0b28f48dda45069155ce4df680c857e5c3a8c0c1',
-        'HOST': 'ec2-52-6-77-239.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
