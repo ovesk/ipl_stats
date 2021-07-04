65682c46d456a61b3d4adda75e4b125a03d83755
Oves Khan <oves.khan@amplussolar.com>
2021-07-04 23:52:41 +0530

    settings file updated

--- ipl/settings.py
+++ ipl/settings.py
@@ -133,4 +133,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'stats/static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
